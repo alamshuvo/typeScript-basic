fdb34d269fd87cc71b9666eddd4cad8b25aba21d
iftakhar <iftakhar@iftakhars-MacBook-Pro.local>
2024-10-07 20:06:34 +0600

    tupple type

---
+++ userDefined-Type/tupleType.js
@@ -0,0 +1,5 @@
+// mixed data type -key ,value fair 
+var user;
+user = [101, "iftakhar"];
+user.push(222, "another");
+console.log(user[0]);
---
+++ userDefined-Type/tupleType.ts
@@ -0,0 +1,6 @@
+// mixed data type -key ,value fair 
+let user:[number,string];
+
+user=[101,"iftakhar"]
+user.push(222,"another")
+console.log(user[0]);
