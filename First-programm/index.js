8659b98f74b6ffe5e431a04e58dcddaa4236dede
iftakhar <iftakhar@iftakhars-MacBook-Pro.local>
2024-10-07 18:59:46 +0600

    first commit

---
+++ First-programm/index.js
@@ -0,0 +1,6 @@
+var userName = "iftakhar Alam";
+var number = 120;
+userName = "Shahin";
+number = 80;
+console.log(userName);
+console.log(number);
---
+++ First-programm/index.ts
@@ -0,0 +1,8 @@
+let userName:string|number="iftakhar Alam"
+let number:number=120;
+
+userName="Shahin"
+number=80
+console.log(userName);
+console.log(number);
+
