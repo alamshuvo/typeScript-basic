5fb759fb639a488032fa5ae2532abd3124cf0683
iftakhar <iftakhar@iftakhars-MacBook-Pro.local>
2024-10-07 20:02:51 +0600

    arraytype

---
+++ userDefined-Type/array-type.js
@@ -0,0 +1,9 @@
+// let userNames:string[];
+var userNames;
+var usersId;
+userNames = ["iftakhar", "alam", "shuvo"];
+console.log(userNames[0]);
+var multipleTypes;
+multipleTypes = [1, "adf"];
+console.log(multipleTypes);
+console.log(userNames.sort());
---
+++ userDefined-Type/array-type.ts
@@ -0,0 +1,9 @@
+// let userNames:string[];
+let userNames:Array<string>
+let usersId:number[]
+userNames=["iftakhar","alam","shuvo"]
+console.log(userNames[0]);
+let multipleTypes:(string|number)[];
+multipleTypes=[1,"adf"];
+console.log(multipleTypes);
+console.log(userNames.sort());
